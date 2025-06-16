786c48fb80c51fa3d5fafe15f62f0f6eff620be1
DivyaVarsha04 <124606187+DivyaVarsha04@users.noreply.github.com>
2025-06-16 20:27:56 +0530

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    first_name=models.CharField(max_length=15)
+    last_name=models.CharField(max_length=15)
+    age=models.IntegerField()
+    course=models.CharField(max_length=20)
+    join_date=models.DateField()
+    phone=models.CharField(max_length=10)
+
+class Employee(models.Model):
+    first_name=models.CharField(max_length=15)
+    last_name=models.CharField(max_length=15)
+    age=models.CharField()
+    phone_no=models.CharField(max_length=10)
+    dept_no=models.CharField(max_length=20)
+    designation=models.CharField(max_length=20)
+    commision=models.FloatField()
+    salary=models.FloatField()
+    address=models.CharField(max_length=30)
+    
